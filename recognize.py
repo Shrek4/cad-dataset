daf984eef05ea0503a24e85df634b15453625a25
Shrek4 <skarrbonov@gmail.com>
2022-09-30 12:57:23 +0300

    python mnist recognition

--- recognize.py
+++ recognize.py
@@ -1,64 +1,48 @@
-import numpy as np
 import cv2
-import matplotlib.pyplot as plt
-import imutils
-import matplotlib.patches as mpatches
-from skimage import data, segmentation, measure, morphology, color
-import tensorflow as tf
+from keras.models import load_model
+import numpy as np
+FILENAME="img.png"
+
+model = load_model('mnist.h5')
+
+def Recognize_Digit(input):
+
+    # image = cv2.imread(FILENAME, cv2.IMREAD_COLOR)
+    image=input
+    gray = cv2.cvtColor(image.copy(), cv2.COLOR_BGR2GRAY)
+    ret, th = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+
+    contours = cv2.findContours(th, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
+
+    for cnt in contours:
+        x, y, w, h = cv2.boundingRect(cnt)
+        # make a rectangle box around each curve
+        cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 1)
+
+        # Cropping out the digit from the image corresponding to the current contours in the for loop
+        digit = th[y:y + h, x:x + w]
+
+        # Resizing that digit to (18, 18)
+        resized_digit = cv2.resize(digit, (18, 18))
+
+        # Padding the digit with 5 pixels of black color (zeros) in each side to finally produce the image of (28, 28)
+        padded_digit = np.pad(resized_digit, ((5, 5), (5, 5)), "constant", constant_values=0)
+
+        digit = padded_digit.reshape(1, 28, 28, 1)
+        digit = digit / 255.0
+
+        pred = model.predict([digit])[0]
+        final_pred = np.argmax(pred)
+
+        data = str(final_pred) + ' ' + str(int(max(pred) * 100)) + '%'
+
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        fontScale = 0.5
+        color = (255, 0, 0)
+        thickness = 1
+        cv2.putText(image, data, (x, y - 5), font, fontScale, color, thickness)
 
 
-class Number_recognition():
-    "" "Инициализация восстановления модели" ""
+    # cv2.imwrite("output.png", image)
+    return image
 
-    def __init__(self, img):
-        self.sess = tf.InteractiveSession()
-        saver = tf.train.import_meta_graph('model/model.meta')
-        saver.restore(self.sess, 'model / model')  # восстановление модели
-        # graph = tf.get_default_graph()
-        # Получить входной тензор и получить выходной тензор
-        self.input_x = self.sess.graph.get_tensor_by_name("Mul:0")
-        self.y_conv2 = self.sess.graph.get_tensor_by_name("final_result:0")
-        self.Preprocessing(img)  # предварительная обработка изображений
-    
-    def recognition(self, im):
-        im = cv2.resize(im, (28, 28), interpolation=cv2.INTER_CUBIC)
-        x_img = np.reshape(im, [-1, 784])
-        output = self.sess.run(self.y_conv2, feed_dict={self.input_x: x_img})
-        print ('Введенное число:% d'% (np.argmax (output)))
-        return np.argmax (output) # вернуть результат распознавания
-    
-    def Preprocessing(self, image):
-        if image.shape[0] > 1000:
-            image = imutils.resize (image, height = 800) # Если изображение слишком большое, локальная пороговая скорость сегментации будет немного медленнее, поэтому понижающая дискретизация будет, когда изображение слишком большое
-        img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  # convert to gray picture
-        m1, n1 = img.shape
-        k = int(m1 / 19) + 1
-        l = int(n1 / 19) + 1
-        #img = cv2.GaussianBlur (img, (3, 3), 0) # фильтр Гаусса
-        imm = img.copy()
-        # Метод локальной пороговой сегментации на основе Niblack лучше для извлечения сегментации текстового изображения
-        for x in range(k):
-            for y in range(l):
-                s = imm[19 * x:19 * (x + 1), 19 * y:19 * (y + 1)]
-                me = s.mean () # среднее
-                var = np.std (s) # дисперсия
-                t = me * (1 - 0.2 * ((125 - var) / 125))
-                ret, imm[19 * x:19 * (x + 1), 19 * y:19 * (y + 1)] = cv2.threshold(imm[19 * x:19 * (x + 1), 19 * y:19 * (y + 1)], t, 255, cv2.THRESH_BINARY_INV)
-                label_image = measure.label (imm) # метка подключенной области
-                for region in measure.regionprops (label_image): # цикл, чтобы получить набор атрибутов каждой подключенной области
-                    # Игнорировать небольшие участки
-                    if region.area < 100:
-                        continue
-                    minr, minc, maxr, maxc = region.bbox # получить параметры прямоугольника аутсорсинга
-                    cv2.rectangle (image, (minc, minr), (maxc, maxr), (0, 255, 0), 2) # нарисовать связанные области
-        im2 = imm [minr-15: maxr + 15, minc-15: maxc + 15] # Получить интересующую область, то есть площадь каждого числа
-        number = self.recognition (im2) # Распознать
-        cv2.putText (image, str (number), (minc, minr-10), 0, 2, (0, 0, 255), 2) # Записать результат распознавания на исходном изображении
-        cv2.imshow("threshold", imm)
-        cv2.imshow("recgonize result", image)
-        cv2.waitKey(0)
-        
-        
-if __name__ == '__main__':
-    img = cv2.imread("./imgs/1.png")
-    x = Number_recognition(img)
--- server.py
+++ server.py
@@ -1,13 +1,22 @@
+from codecs import unicode_escape_decode, unicode_escape_encode
 import threading
-import requests
 import argparse
-from flask import Flask
+from flask import Flask, send_from_directory, request
 import csv
 import json
+from flask_cors import CORS
+import os
+import cv2
+from werkzeug.utils import secure_filename
+
+from recognize import Recognize_Digit
+PORT=3001
 
 app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+CORS(app)
 
-parts = []
+
 
 
 def getParts():
@@ -23,14 +32,97 @@ def getParts():
             jsonArray.append(row)
     return jsonArray
 
+def getStandarts():
+    jsonArray = []
+    #read csv file
+    with open("Standarts.csv", encoding='utf-8') as csvf: 
+        #load csv file data using csv library's dictionary reader
+        csvReader = csv.DictReader(csvf, delimiter=";") 
+
+        #convert each csv row into python dict
+        for row in csvReader: 
+            #add this python dict to json array
+            jsonArray.append(row)
+    return jsonArray
+
+
+def getClasses():
+    jsonArray = []
+    #read csv file
+    with open("Classes.csv", encoding='utf-8') as csvf: 
+        #load csv file data using csv library's dictionary reader
+        csvReader = csv.DictReader(csvf, delimiter=";") 
+
+        #convert each csv row into python dict
+        for row in csvReader: 
+            #add this python dict to json array
+            jsonArray.append(row)
+    return jsonArray
+
+def getImCat():
+    jsonArray = []
+    #read csv file
+    with open("Images_categories.csv", encoding='utf-8') as csvf: 
+        #load csv file data using csv library's dictionary reader
+        csvReader = csv.DictReader(csvf, delimiter=";") 
+
+        #convert each csv row into python dict
+        for row in csvReader: 
+            #add this python dict to json array
+            jsonArray.append(row)
+    return jsonArray
+
+
+
+parts=getParts()
+classes=getClasses()
+standarts=getStandarts()
+im_categories=getImCat()
+
+def getClass(class_id):
+    return [x for x in classes if x['id'] == class_id][0]['name']
+
+def getStandart(standart_id):
+    return [x for x in standarts if x['id'] == standart_id][0]['name']
+
+
+def getImages(part_id):
+    cat_id=[x for x in parts if x['id'] == part_id][0]['image_category']
+    cat_name=[x for x in im_categories if x['id'] == cat_id][0]['folder']
+    return [cat_name+'/'+x for x in os.listdir(cat_name)]
 
 @app.route('/parts')
 def get_parts():
     try:
-     return getParts()
+        data=[]
+        for i in range(len(parts)):
+            data.append({"id": parts[i]['id'], "class": getClass(parts[i]['class_id']), "standart": getStandart(parts[i]['standart_id']), "size": parts[i]['size'], "images": getImages(parts[i]['id'])})
+        return data
     except Exception as e:
         print(e)
 
+@app.route('/Images/<path:path>')
+def get_files(path):
+    try:
+        return send_from_directory("Images", path)
+    except Exception as e:
+        print(e)
+
+@app.route('/recognize_img/<path:path>')
+def get_recognized_img(path):
+    try:
+        return send_from_directory("recognize_img", path)
+    except Exception as e:
+        print(e)
+
+@app.route('/upload', methods=['POST'])
+def fileUpload():
+    file_dir="recognize_img/"
+    f = request.files['file']
+    f.save(file_dir+"input.png")
+    recognized=Recognize_Digit(cv2.imread(file_dir+"input.png", cv2.IMREAD_COLOR))
+    cv2.imwrite(file_dir+"output.png", recognized)
+    return {"url": file_dir+"output.png"}
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="localhost", port=PORT)
