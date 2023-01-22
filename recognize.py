d7450a2f6a959bb4e2b7b5289f90a91ed60dcf9f
Михаил Строганов <skarrbonov@gmail.com>
2023-01-22 23:41:04 +0300

    finally, obamium

--- recognize.py
+++ recognize.py
@@ -1,9 +1,43 @@
 import cv2
 from keras.models import load_model
 import numpy as np
+from PIL import Image
 
 
 model = load_model('cadmnist.h5')
+
+def make_square(img, max_size, fill_color):
+    # find image dimensions
+    old_img = Image.fromarray(img)
+    size = (min(max_size, max(old_img.size)),) * 2
+
+    # resize if old image is larger than max_size
+    if size[0] < old_img.size[0] or size[1] < old_img.size[1]:
+        old_img.thumbnail(size)
+
+    # create new image with the given color and computed size
+    new_img = Image.new(old_img.mode, size, fill_color)
+
+    # find coordinates of upper-left corner to center the old image in the new image
+    assert new_img.size[0] >= old_img.size[0]
+    assert new_img.size[1] >= old_img.size[1]
+
+    x = (new_img.size[0] - old_img.size[0]) // 2
+    y = (new_img.size[1] - old_img.size[1]) // 2
+
+    # paste image
+    new_img.paste(old_img, (x, y))
+
+    # save image
+    if(max(new_img.size)<150):
+        new_img=new_img.resize((150,150))
+    return np.array(new_img)
+
+def prepare(img):
+    padded= np.pad(img, ((5, 5), (5, 5)), "constant", constant_values=0)
+    squared=make_square(padded, 150, 0)
+    ret, th = cv2.threshold(squared, 0, 255, cv2.THRESH_OTSU)
+    return th
 
 def Recognize_Digit(input):
 
@@ -20,16 +54,21 @@ def Recognize_Digit(input):
         cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 1)
 
         # Cropping out the digit from the image corresponding to the current contours in the for loop
-        digit = th[y:y + h, x:x + w]
+        # digit = th[y:y + h, x:x + w]
 
-        # Resizing that digit to (18, 18)
-        resized_digit = cv2.resize(digit, (140, 140))
+        # # Resizing that digit to (18, 18)
+        # resized_digit = cv2.resize(digit, (140, 140))
 
-        # Padding the digit with 5 pixels of black color (zeros) in each side to finally produce the image of (28, 28)
-        padded_digit = np.pad(resized_digit, ((5, 5), (5, 5)), "constant", constant_values=0)
+        # # Padding the digit with 5 pixels of black color (zeros) in each side to finally produce the image of (28, 28)
+        # padded_digit = np.pad(resized_digit, ((5, 5), (5, 5)), "constant", constant_values=0)
 
-        # digit = padded_digit.reshape(1, 28, 28, 1)
-        digit = padded_digit.reshape(1, 150, 150, 1)
+        # # digit = padded_digit.reshape(1, 28, 28, 1)
+        # digit = padded_digit.reshape(1, 150, 150, 1)
+
+        print(np.shape(th[y:y + h, x:x + w]))
+        digit=prepare(th[y:y + h, x:x + w])
+        print(np.shape(digit))
+        digit=digit.reshape(1,150,150,1)
         digit = digit / 255.0
 
         pred = model.predict([digit])[0]
